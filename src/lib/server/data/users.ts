9e7f59e0a9a52a1312a9f0077a11f6ecde6f96b3
fseda <fsra1610@gmail.com>
2024-09-17 13:45:43 -0300

    User functions

--- src/lib/server/data/users.ts
+++ src/lib/server/data/users.ts
@@ -4,10 +4,17 @@ import { users } from "../db/schema/users";
 
 type User = typeof users.$inferSelect;
 type NewUser = typeof users.$inferInsert;
+type PublicUser = Pick<User, 'id' | 'username' | 'avatar' | 'avatar_url' | 'bio'>
 
 export async function getUserByEmail(email: string): Promise<User | undefined> {
   return await db.query.users.findFirst({
     where: eq(users.email, email),
+  });
+}
+
+export async function getUserById(id: string): Promise<User | undefined> {
+  return await db.query.users.findFirst({
+    where: eq(users.id, id),
   });
 }
 
@@ -17,4 +24,9 @@ export async function createUser(newUser: NewUser): Promise<User>  {
 
 export async function getOrCreateUserByEmail(email: string): Promise<User> {
   return await getUserByEmail(email) || await createUser({ email, status: 'active' });
+}
+
+export function getPublicUser(user: User): PublicUser {
+  const { id, username, avatar, avatar_url, bio } = user;
+  return { id, username, avatar, avatar_url, bio };
 }
