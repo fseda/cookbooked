4328a3a0790d354256cec0dfd4f833b99b0a6c54
fseda <fsra1610@gmail.com>
2024-09-26 18:55:41 -0300

    h1 header

---
+++ src/lib/components/typography/H1.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+	import type { Snippet } from "svelte";
+
+  let {
+    children,
+    class: clazz
+  }: {
+    children: Snippet<[]>,
+    class?: string,
+  } = $props();
+</script>
+
+<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl {clazz}">
+  {@render children()}
+</h1>
