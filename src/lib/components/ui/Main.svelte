d78b066a33b955360b2308a0c65b63f585b21715
fseda <fsra1610@gmail.com>
2024-09-25 22:12:23 -0300

    Unused imports

--- src/lib/components/ui/Main.svelte
+++ src/lib/components/ui/Main.svelte
@@ -1,5 +1,7 @@
 <script lang=ts>
-	import type { Snippet } from "svelte";
+	import { page } from "$app/stores";
+	import { setContext, type Snippet } from "svelte";
+	import { writable } from "svelte/store";
 
   interface Props {
     class?: string;
@@ -11,7 +13,6 @@
     children,
   }: Props = $props();
 
-  
 </script>
 
 <main class="relative h-full w-full overflow-y-auto flex justify-center items-center p-4 {clazz}">
